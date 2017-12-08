eb4b6ce575ae4513268ee7165f59692c809abbaf
Mohammad Sohail <contactsohail07@gmail.com>
2017-12-08 17:03:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,41 +1,58 @@
 from socket import *
 import os
+import time
 s= socket()
 print("socket successfully created")
 
 def req_file(path):
 	sp=path.split()
-	if sp[2][:4]=="HTTP":
+	print(sp)
+	if  sp[2][:4]=="HTTP":
 		if os.path.isfile(sp[1]): 
 			f=open(sp[1])
-			a = f.read()
+			a=f.read()
 			ans=""
-			ans+="200 OK\n"
+			ans+="HTTP/1.1 200 OK\r\n"
 			if sp[1][-2:]=="xt" or sp[1][-2:]=="ml":
-				ans+="Content-Type : text/html\n"
+				ans+="Content-Type : text/html\r\n"
 			else:
-				ans+="Content-Type : image\n"
+				ans+="Content-Type : image\r\n"
+			ans+="Content-Length : "+ str(len(a))+ "\r\n"
 			if sp[2][-1]=="0":
-				ans+="Connection : Close\n"
+				ans+="Connection : Close\r\n\r\n"
 			else:
-				ans+="Connection : Keep-Alive\n"
-			ans+=a
+				ans+="Connection : Keep-Alive\r\n\r\n"
 			return ans
 		else:
-			print("404 Not Found")
+			return "HTTP/1.1 404 Not Found\r\n\r\n"
 	else:
-		print("403 Forbidden")	
+		return "HTTP/1.1 403 Forbidden\r\n\r\n"
 
+def content(path):
+	sp=path.split()
+	if os.path.isfile(sp[1]): 
+		f=open(sp[1])
+		a=f.read()
+		return a 
+	else:
+		return "HTTP/1.1 404 Not Found\r\n\r\n"
 
-s.bind(('', 2006))
+s.bind(('127.0.0.1', 8080))
 s.listen(5)
 print("socket is listening")
 while True:
 	addr='127.0.0.1'
 	c,addr=s.accept()
 	print("got connection from" , addr)
-	req=c.recv(1024)
-	r_f=req_file(req)
-	c.send(r_f)
-	c.close()
+	req=c.recv(1000)
+	pid=os.fork()
+	if pid==0:
+		time.sleep(5)
+		r_f=req_file(req)
+		con=content(req)
+		c.send(r_f)
+		c.send(con)
+		c.close()
+	else:
+		c.close()
 
